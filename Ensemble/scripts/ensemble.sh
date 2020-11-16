f44a22a4fbeed42ffed69382d0193a90ea279000
usef-kh <yousif.khaireddin@gmail.com>
2020-11-16 00:13:38 -0500

    cleanup

--- Ensemble/scripts/ensemble.sh
+++ Ensemble/scripts/ensemble.sh
@@ -2,7 +2,7 @@
 
 #$ -N ensemble			# Job name
 #$ -P ec523			# Project name
-#$ -o outputs/ensemble		# Output file name
+#$ -o ../outputs/ensemble	# Output file name
 #$ -pe omp 2		
 #$ -l gpus=1 		
 #$ -l gpu_c=6 	
@@ -12,5 +12,7 @@
 module load miniconda/4.7.5
 conda activate ec523
 export PYTHONPATH=/projectnb/ykh/project/Ensemble/:$PYTHONPATH
+
+cd ..
 
 python train.py type=ensemble name=ensemble n_epochs=1 subnet_type=tuned sub1_path='checkpoints/sub1_tuned/epoch_300' sub2_path='checkpoints/sub2_tuned/epoch_300' sub3_path='checkpoints/sub3_tuned/epoch_300'
--- Ensemble/scripts/sub2.sh
+++ Ensemble/scripts/sub2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
-#$ -N sub3			# Job name
+#$ -N sub2			# Job name
 #$ -P ec523			# Project name
-#$ -o outputs/sub3		# Output file name
+#$ -o ../outputs/sub2		# Output file name
 #$ -pe omp 2		
 #$ -l gpus=1 		
 #$ -l gpu_c=6 	
@@ -13,4 +13,6 @@ module load miniconda/4.7.5
 conda activate ec523
 export PYTHONPATH=/projectnb/ykh/project/Ensemble/:$PYTHONPATH
 
-python train.py type=subnet name=sub3_tuned
+cd ..
+
+python train.py type=subnet name=sub2_tuned
