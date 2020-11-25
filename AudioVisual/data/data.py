9078d1434ec19e3515e69c2ae18168c04abafdac
usef-kh <yousif.khaireddin@gmail.com>
2020-11-25 18:59:04 -0500

    cleanup

---
+++ AudioVisual/data/data.py
@@ -0,0 +1,158 @@
+import collections
+
+import numpy as np
+import torch
+from sklearn.model_selection import train_test_split
+from torchvision.transforms import transforms
+
+from data.processor import process_video, process_audio
+
+
+def prepare_gender():
+    gender_mapping = {'m': 0, 'f': 1}
+
+    gender = {}
+    with open("../../datasets/enterface/gender.txt") as txtfile:
+        for line in txtfile:
+            (subject_id, subject_gender) = line.split()
+            gender[subject_id] = gender_mapping[subject_gender]
+
+    return gender
+
+
+def get_subject_id(path):
+    i = path.find("subject ") + 8
+
+    idx = ''
+    while path[i].isdigit():
+        idx += path[i]
+        i += 1
+
+    return idx
+
+
+def split(dataset):
+    train = collections.defaultdict(list)
+    test = collections.defaultdict(list)
+    val = collections.defaultdict(list)
+
+    for emotion, paths in dataset.items():
+        # 0.25 * 0.8 = 0.2
+        train_paths, test_paths = train_test_split(paths, test_size=0.2, random_state=1, shuffle=True)
+        train_paths, val_paths = train_test_split(train_paths, test_size=0.25, random_state=1, shuffle=True)
+
+        train[emotion].extend(train_paths)
+        val[emotion].extend(val_paths)
+        test[emotion].extend(test_paths)
+
+    return train, val, test
+
+
+def prepare_paths(video_dir='../../datasets/enterface/original', audio_dir='../../datasets/enterface/wav'):
+    paths = collections.defaultdict(list)
+
+    possible_emotions = ['anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise']
+    emotion_mapping = {emotion: i for i, emotion in enumerate(possible_emotions)}
+
+    for curr_dir, sub_dir, files in os.walk(video_dir):
+        if files:
+            emotion = os.path.split(os.path.split(curr_dir)[0])[-1]
+
+            # catch the exception in folder structure from subject 6
+            if emotion not in emotion_mapping:
+                emotion = os.path.split(curr_dir)[1]
+
+            files = [os.path.join(curr_dir, file) for file in files if file[-2:] != 'db']
+
+            emotion_id = emotion_mapping[emotion]
+            paths[emotion_id].extend(files)
+
+    path_tuples = collections.defaultdict(list)
+
+    for emotion, avi_paths in paths.items():
+        for avi_path in avi_paths:
+            wav_file = avi_path[len(video_dir) + 1:][:-3] + 'wav'
+            wav_path = os.path.join(audio_dir, wav_file)
+
+            path_tuples[emotion].append((avi_path, wav_path))
+
+    return split(path_tuples)
+
+
+def prepare_data(data):  # data type will be dictionary,  emotion:  path.
+
+    gender_mapping = prepare_gender()
+    frames, specs, gender, labels = [], [], [], []
+    for emotion_id, paths in data.items():
+        for avi_path, wav_path in paths:
+
+            key_frames = process_video(avi_path)
+            spectrograms = process_audio(wav_path)
+
+            assert (key_frames is None) == (spectrograms is None), "Processors must accept/reject the same paths"
+
+            if key_frames is not None and spectrograms is not None:
+                if frames == []:
+                    frames = key_frames
+                    specs = spectrograms
+                else:
+
+                    assert len(key_frames) == len(spectrograms), "Processors must create the same number of samples"
+                    frames = np.vstack((frames, key_frames))
+                    specs = np.vstack((specs, spectrograms))
+
+                subject_id = get_subject_id(wav_path)  # or avi path, its the same
+                gender_id = gender_mapping[subject_id]
+
+                labels += [emotion_id] * len(key_frames)
+                gender += [gender_id] * len(key_frames)
+
+    labels = np.array(labels)
+    gender = np.array(gender)
+
+    print("frame dims", frames.shape)
+    print("specs dims", specs.shape)
+    print("label dims", labels.shape)
+    print("gender dims", gender.shape)
+
+    return (frames, specs), (gender, labels)
+
+
+def get_dataloaders(video_dir=None, audio_dir=None):
+    train_paths, val_paths, test_paths = prepare_paths(video_dir, audio_dir)
+
+    xtrain, ytrain = prepare_data(train_paths)
+    xval, yval = prepare_data(val_paths)
+    xtest, ytest = prepare_data(test_paths)
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    # train = CustomDataset(xtrain, ytrain, transform)
+    # val = CustomDataset(xval, yval, transform)
+    # test = CustomDataset(xtest, ytest, transform)
+
+    # trainloader = DataLoader(train, batch_size=1, shuffle=True)  # , num_workers=2)
+    # valloader = DataLoader(val, batch_size=1, shuffle=True)  # , num_workers=2)
+    # testloader = DataLoader(test, batch_size=1, shuffle=True)  # , num_workers=2)
+
+    # return trainloader, valloader, testloader
+
+
+video_dir = '../../datasets/enterface/original'
+audio_dir = '../../datasets/enterface/wav'
+train_paths, val_paths, test_paths = prepare_paths(video_dir, audio_dir)
+
+print("Train")
+xtrain, ytrain = prepare_data(train_paths)
+
+print("Val")
+xval, yval = prepare_data(val_paths)
+
+print("Test")
+xtest, ytest = prepare_data(test_paths)
+
+torch.save((xtrain, ytrain), 'train')
+torch.save((xval, yval), 'val')
+torch.save((xtest, ytest), 'test')
--- AudioVisual/data/processor.py
+++ AudioVisual/data/processor.py
@@ -1,65 +1,11 @@
-import collections
-import os
-
 import cv2
 import librosa
 import librosa.display
 import numpy as np
-import torch
-import torchvision.transforms as transforms
+
 from librosa.feature import melspectrogram
 from scipy.stats import chisquare
-from sklearn.model_selection import train_test_split
 
-
-# from data.dataset import CustomDataset
-
-
-def split(dataset):
-    train = collections.defaultdict(list)
-    test = collections.defaultdict(list)
-    val = collections.defaultdict(list)
-
-    for emotion, paths in dataset.items():
-        # 0.25 * 0.8 = 0.2
-        train_paths, test_paths = train_test_split(paths, test_size=0.2, random_state=1, shuffle=True)
-        train_paths, val_paths = train_test_split(train_paths, test_size=0.25, random_state=1, shuffle=True)
-
-        train[emotion].extend(train_paths)
-        val[emotion].extend(val_paths)
-        test[emotion].extend(test_paths)
-
-    return train, val, test
-
-def prepare_paths(video_dir='../../datasets/enterface/original', audio_dir='../../datasets/enterface/wav'):
-    paths = collections.defaultdict(list)
-
-    possible_emotions = ['anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise']
-    emotion_mapping = {emotion: i for i, emotion in enumerate(possible_emotions)}
-
-    for curr_dir, sub_dir, files in os.walk(video_dir):
-        if files:
-            emotion = os.path.split(os.path.split(curr_dir)[0])[-1]
-
-            # catch the exception in folder structure from subject 6
-            if emotion not in emotion_mapping:
-                emotion = os.path.split(curr_dir)[1]
-
-            files = [os.path.join(curr_dir, file) for file in files if file[-2:] != 'db']
-
-            emotion_id = emotion_mapping[emotion]
-            paths[emotion_id].extend(files)
-
-    path_tuples = collections.defaultdict(list)
-
-    for emotion, avi_paths in paths.items():
-        for avi_path in avi_paths:
-            wav_file = avi_path[len(video_dir) + 1:][:-3] + 'wav'
-            wav_path = os.path.join(audio_dir, wav_file)
-
-            path_tuples[emotion].append((avi_path, wav_path))
-
-    return split(path_tuples)
 
 def face_detection(frame):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -73,6 +19,7 @@ def face_detection(frame):
     resized_face = cv2.resize(face, (277, 277), interpolation=cv2.INTER_AREA)
 
     return resized_face
+
 
 def get_keyframes(chunk, shift=4, window_len=7):
     n_keyframes = len(chunk) // 4
@@ -94,6 +41,7 @@ def get_keyframes(chunk, shift=4, window_len=7):
         keyframes[i, :, :] = keyframe
 
     return keyframes
+
 
 def process_video(path):
     video = cv2.VideoCapture(path)
@@ -136,6 +84,7 @@ def process_video(path):
         chunk_keys[i] = frames
 
     return chunk_keys
+
 
 def process_audio(path):
     y, sr = librosa.load(path, sr=None)
@@ -181,100 +130,5 @@ def process_audio(path):
 
     return features
 
-def get_subject_id(path):
-    i = path.find("subject ") + 8
-
-    idx = ''
-    while path[i].isdigit():
-        idx += path[i]
-        i += 1
-
-    return idx
-
-def prepare_gender():
-    gender_mapping = {'m': 0, 'f': 1}
-
-    gender = {}
-    with open("../../datasets/enterface/gender.txt") as txtfile:
-        for line in txtfile:
-            (subject_id, subject_gender) = line.split()
-            gender[subject_id] = gender_mapping[subject_gender]
-
-    return gender
-
-def prepare_data(data):  # data type will be dictionary,  emotion:  path.
-
-    gender_mapping = prepare_gender()
-    frames, specs, gender, labels = [], [], [], []
-    for emotion_id, paths in data.items():
-        for avi_path, wav_path in paths:
-
-            key_frames = process_video(avi_path)
-            spectrograms = process_audio(wav_path)
-
-            assert (key_frames is None) == (spectrograms is None), "Processors must accept/reject the same paths"
-
-            if key_frames is not None and spectrograms is not None:
-                if frames == []:
-                    frames = key_frames
-                    specs = spectrograms
-                else:
-
-                    assert len(key_frames) == len(spectrograms), "Processors must create the same number of samples"
-                    frames = np.vstack((frames, key_frames))
-                    specs = np.vstack((specs, spectrograms))
-
-                subject_id = get_subject_id(wav_path)  # or avi path, its the same
-                gender_id = gender_mapping[subject_id]
-
-                labels += [emotion_id] * len(key_frames)
-                gender += [gender_id] * len(key_frames)
-
-    labels = np.array(labels)
-    gender = np.array(gender)
-
-    print("frame dims", frames.shape)
-    print("specs dims", specs.shape)
-    print("label dims", labels.shape)
-    print("gender dims", gender.shape)
-
-    return (frames, specs), (gender, labels)
-
-def get_dataloaders(video_dir=None, audio_dir=None):
-    train_paths, val_paths, test_paths = prepare_paths(video_dir, audio_dir)
-
-    xtrain, ytrain = prepare_data(train_paths)
-    xval, yval = prepare_data(val_paths)
-    xtest, ytest = prepare_data(test_paths)
-
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-    ])
-
-    # train = CustomDataset(xtrain, ytrain, transform)
-    # val = CustomDataset(xval, yval, transform)
-    # test = CustomDataset(xtest, ytest, transform)
-
-    # trainloader = DataLoader(train, batch_size=1, shuffle=True)  # , num_workers=2)
-    # valloader = DataLoader(val, batch_size=1, shuffle=True)  # , num_workers=2)
-    # testloader = DataLoader(test, batch_size=1, shuffle=True)  # , num_workers=2)
-
-    # return trainloader, valloader, testloader
 
 
-video_dir = '../../datasets/enterface/original'
-audio_dir = '../../datasets/enterface/wav'
-train_paths, val_paths, test_paths = prepare_paths(video_dir, audio_dir)
-
-print("Train")
-xtrain, ytrain = prepare_data(train_paths)
-
-print("Val")
-xval, yval = prepare_data(val_paths)
-
-print("Test")
-xtest, ytest = prepare_data(test_paths)
-
-torch.save((xtrain, ytrain), 'train')
-torch.save((xval, yval), 'val')
-torch.save((xtest, ytest), 'test')
