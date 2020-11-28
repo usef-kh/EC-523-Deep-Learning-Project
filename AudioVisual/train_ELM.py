977c4461faeb16d1335934dfe474d9d4e73a1340
usef-kh <yousif.khaireddin@gmail.com>
2020-11-28 00:29:41 -0500

    Train Final ELM

--- AudioVisual/train_ELM.py
+++ AudioVisual/train_ELM.py
@@ -7,11 +7,11 @@ from torch.utils.data import DataLoader
 
 from data.dataset import CustomDataset
 from models.CNNs import CNN_2DFeatures, CNN_3DFeatures
-from models.ELM import ELM
+from models.ELM import ELM, ELMFeatures
 from models.pseudoInverse import pseudoInverse
 
-#cpu = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-cpu = gpu = "cpu"
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = "cpu"
 warnings.filterwarnings("ignore")
 
 
@@ -37,9 +37,10 @@ def train_ELM(model, optimizer, train_loader):
     for batch_idx, sample in enumerate(train_loader):
         x_keyframes, x_specs, y_gender, y_emotion = sample
 
-        data = passThroughCNNs(x_keyframes, x_specs)
+        data = passThroughCNNs(x_keyframes, x_specs)  # CNNs
+        data = elm1(data)  # Gender ELM
 
-        data, target = data.to(gpu), y_gender.to(gpu)
+        data, target = data.to(device), y_emotion.to(device)
 
         data, target = Variable(data, requires_grad=False, volatile=True), Variable(target, requires_grad=False,
                                                                                     volatile=True)
@@ -63,9 +64,11 @@ def test(model, test_loader):
     for batch_idx, sample in enumerate(test_loader):
         x_keyframes, x_specs, y_gender, y_emotion = sample
 
-        data = passThroughCNNs(x_keyframes, x_specs)
+        data = passThroughCNNs(x_keyframes, x_specs)  # CNNs
+        data = elm1(data)  # Gender ELM
 
-        data, target = data.to(gpu), y_gender.to(gpu)
+        data, target = data.to(device), y_emotion.to(device)
+
         data, target = Variable(data, requires_grad=False, volatile=True), Variable(target, requires_grad=False,
                                                                                     volatile=True)
 
@@ -82,9 +85,9 @@ def test(model, test_loader):
 
 
 def passThroughCNNs(keyframes, specs):
-    x_vid = cnn3d(keyframes.to(cpu))
-    x_aud = cnn2d(specs.to(gpu))
-    x = torch.cat((x_aud.to(cpu), x_vid), dim=1)
+    x_vid = cnn3d(keyframes.to(device))
+    x_aud = cnn2d(specs.to(device))
+    x = torch.cat((x_aud.to(device), x_vid), dim=1)
 
     return x
 
@@ -93,14 +96,20 @@ def passThroughCNNs(keyframes, specs):
 print("Building 2D CNN")
 cnn2d_path = r"/projectnb/ec523/ykh/project/AudioVisual/checkpoints/cnn2d/epoch_300"
 cnn2d_params = torch.load(cnn2d_path)['params']
-cnn2d = CNN_2DFeatures().to(gpu)
+cnn2d = CNN_2DFeatures().to(device)
 load_features(cnn2d, cnn2d_params)
 
 print("Building 3D CNN")
 cnn3d_path = r"/projectnb/ec523/ykh/project/AudioVisual/checkpoints/cnn3d/epoch_300"
 cnn3d_params = torch.load(cnn3d_path)['params']
-cnn3d = CNN_3DFeatures().to(cpu)
+cnn3d = CNN_3DFeatures().to(device)
 load_features(cnn2d, cnn2d_params)
+
+# Building Gender ELM
+print("Building Gender ELM")
+elm1_params = torch.load("elm1").state_dict()  # I accidentally saved the whole model rather than the state dict
+elm1 = ELMFeatures(input_size=8192, hidden_size=100, num_classes=2, device=device).to(device)
+load_features(elm1, elm1_params)
 
 # Building dataset
 print("Loading Dataset")
@@ -120,25 +129,22 @@ testloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
 
 # ELM
 print("Initializing ELM")
-num_classes = 2
-input_size = 8192
+num_classes = 6  # 'anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise'
+input_size = 100  # output of prev elm
 hidden_size = 100
 
-#model = ELM(input_size, hidden_size, num_classes, device=gpu).to(gpu)
-#optimizer = pseudoInverse(model.parameters(), C=0.001, L=0)
+model = ELM(input_size, hidden_size, num_classes, device=device).to(device)
+optimizer = pseudoInverse(model.parameters(), C=0.001, L=0)
 
-#print("Training ELM")
-#train_ELM(model, optimizer, trainloader)
-
-model = torch.load("ELM")
+print("Training ELM")
+train_ELM(model, optimizer, trainloader)
 
 print("Saving Model")
-torch.save(model, 'ELM')
+torch.save(model, 'Final_ELM')
 
 print("Evaluating Model")
-print("Val")
-test(model, valloader)
+# print("Val")
+# test(model, valloader)
 
 print("Test")
 test(model, testloader)
-
