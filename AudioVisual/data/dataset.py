3b392a84d0a4e4b7300059b85c079bf92b80fcbc
Marco Chan <zhuofachen@gmail.com>
2020-11-29 20:06:06 -0500

    comment

--- AudioVisual/data/dataset.py
+++ AudioVisual/data/dataset.py
@@ -4,20 +4,25 @@ from torch.utils.data import Dataset
 
 class CustomDataset(Dataset):
     def __init__(self, X, Y):
-        self.X = X
-        self.Y = Y
+        # x_keyframes = [12, 277, 277], x_specs = [25, 101], y_gender = [1], y_emotion = [1].
+        self.keyframes = X[0] # [n, 12, 277, 277]
+        self.specs = X[1]     # [n, 25, 101]
+        self.gender = Y[0]    # [n, 1]
+        self.emotion = Y[1]   # [n, 1]
         # self.transform = transform            (frames, specs), (gender, labels)
 
     def __len__(self):
-        return len(self.X[0])
+        # X is a tuple: (frames, specs), X[0] refers to frames, so len(X[0] get the total length of data set, which is n.
+        return len(self.keyframes)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        x_keyframes = torch.from_numpy(self.X[0][idx]).type(torch.float)
-        x_specs = torch.from_numpy(self.X[1][idx]).type(torch.float)
+        x_keyframes = torch.from_numpy(self.keyframes[idx]).type(torch.float)
+        x_specs = torch.from_numpy(self.specs[idx]).type(torch.float)
 
+        # for data augmentation.
         # if self.transform:
         #     x_specs = self.transform(x_specs)
         #
@@ -25,9 +30,8 @@ class CustomDataset(Dataset):
         #     for i in range(n_frames):
         #         x_frames
 
-        y_gender = torch.tensor(self.Y[0][idx]).type(torch.long)
-        y_emotion = torch.tensor(self.Y[1][idx]).type(torch.long)
-
+        y_gender = torch.tensor(self.gender[idx]).type(torch.long)
+        y_emotion = torch.tensor(self.emotion[idx]).type(torch.long)
         sample = (x_keyframes, x_specs, y_gender, y_emotion)
 
         return sample
--- AudioVisual/data/enterface.py
+++ AudioVisual/data/enterface.py
@@ -12,6 +12,11 @@ from data.processor import process_video, process_audio
 
 
 def prepare_gender():
+    """
+    load the gender file and mapping male and female to 0 and 1.
+    :return: dictionary mapping subject ID to gender [0, 1]
+    """
+
     gender_mapping = {'m': 0, 'f': 1}
 
     gender = {}
@@ -24,10 +29,15 @@ def prepare_gender():
 
 
 def get_subject_id(path):
-
+    """
+    :param path: the path of the wav/avi file that contains the subject ID
+    :return: subject ID, string:
+    """
+    # locate the index of the word 'subject', shift by 8, which will be the index of subject ID.
     i = path.find("subject ") + 8
 
     idx = ''
+    #  keep reading digits for the subject id
     while path[i].isdigit():
         idx += path[i]
         i += 1
@@ -36,6 +46,11 @@ def get_subject_id(path):
 
 
 def split(dataset):
+    """
+    split the dataset into train, test, and valudation.with respect to file path.
+    :param dataset: dictionary mapping emotion id to list of path tuples (video path avi, audio path wav)
+    :return: (train, val, test) each is a dictionary mapping emotion id to list of path tuples (video path avi,audio path wav)
+    """
     train = collections.defaultdict(list)
     test = collections.defaultdict(list)
     val = collections.defaultdict(list)
@@ -53,27 +68,38 @@ def split(dataset):
 
 
 def prepare_paths(video_dir='../../datasets/enterface/original', audio_dir='../../datasets/enterface/wav'):
+    """
+    get the path of video file and its corresponding path of audio file, put into a tuple
+    put all the data  into a dicitonary: {emotion ID: file path tuples}
+    split into train, test, validation
+    :param video_dir: path of the video directory.
+    :param audio_dir: path of the audio directory
+    :return: train, test, validation, each is a dictionary mapping emotion id to list of path tuples (video path avi,audio path wav)
+    """
     paths = collections.defaultdict(list)
-
+    # encode the emotion into integers from 0 to 5.
     possible_emotions = ['anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise']
     emotion_mapping = {emotion: i for i, emotion in enumerate(possible_emotions)}
 
     for curr_dir, sub_dir, files in os.walk(video_dir):
         if files:
-            './../datasets/enterface/wav\subject 1\anger\garbage.wav'
+           # path example: './../datasets/enterface/wav\subject 1\anger\garbage.wav'
+           # emotion will be the second section from the right of the path string.
             emotion = os.path.split(os.path.split(curr_dir)[0])[-1]
 
             # catch the exception in folder structure from subject 6
             if emotion not in emotion_mapping:
+                # emotion will be the first section from the right.
                 emotion = os.path.split(curr_dir)[1]
-
+            # get the absolute path of the avi files only, ignore the db files
             files = [os.path.join(curr_dir, file) for file in files if file[-2:] != 'db']
 
             emotion_id = emotion_mapping[emotion]
+            # put in the list.
             paths[emotion_id].extend(files)
 
     path_tuples = collections.defaultdict(list)
-
+    # convert all avi path to wav path, because they have the same directory structure.
     for emotion, avi_paths in paths.items():
         for avi_path in avi_paths:
             wav_file = avi_path[len(video_dir) + 1:][:-3] + 'wav'
@@ -85,13 +111,19 @@ def prepare_paths(video_dir='../../datasets/enterface/original', audio_dir='../.
 
 
 def prepare_data(data):  # dataold type will be dictionary,  emotion:  path.
+    """
+    retrieve the frame data and audio spectrogram from the path tuples
+    :param data: emotion ID: path tuples (video path avi,audio path wav)
+    :return: (frames, specs), (gender, labels)
+    """
 
     gender_mapping = prepare_gender()
 
     frames, specs, gender, labels = [], [], [], []
     for emotion_id, paths in data.items():
         for avi_path, wav_path in paths:
-
+            # get the key frames of the avi_path
+            # get the spectrograms of the wav path
             key_frames = process_video(avi_path)
             spectrograms = process_audio(wav_path)
 
@@ -126,6 +158,12 @@ def prepare_data(data):  # dataold type will be dictionary,  emotion:  path.
 
 
 def get_dataloaders(data_dir="../datasets/enterface/processed/", bs=32):
+    """
+     load preprocess data into train, test, and validation. and create their data loaders baesd on batch size
+    :param data_dir: locate of preprocess data.
+    :param bs: batch size.
+    :return: train, val, test data loaders
+    """
     xtrain, ytrain = torch.load(os.path.join(data_dir, 'train'))
     xval, yval = torch.load(os.path.join(data_dir, 'val'))
     xtest, ytest = torch.load(os.path.join(data_dir, 'test'))
