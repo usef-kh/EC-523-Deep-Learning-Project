3e7baddeaf514c7fef0d7bc5b694b270b41fa058
Yousif Khaireddin <46129961+usef-kh@users.noreply.github.com>
2020-12-10 12:59:38 -0500

    Update sub2.sh

--- ensemble/scripts/sub2.sh
+++ ensemble/scripts/sub2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -l
 
-#$ -N sub2			        # Job name
-#$ -P ec523			        # Project name
-#$ -o outputs/sub2_basic		# Output file name
-#$ -pe omp 2		
-#$ -l gpus=1 		
-#$ -l gpu_c=6 	
-#$ -m ea			# Email on end or abort
-#$ -j y				# Merge output and error file
- 
+#$ -N sub2                              # Job name
+#$ -P ec523                             # Project name
+#$ -o outputs/sub2                      # Output file name
+#$ -pe omp 2
+#$ -l gpus=1
+#$ -l gpu_c=6
+#$ -m ea                        # Email on end or abort
+#$ -j y                         # Merge output and error file
+
 module load miniconda/4.7.5
 conda activate ec523
 export PYTHONPATH=/projectnb/ykh/project/ensemble/:$PYTHONPATH
 
 cd ..
 
-python simpletrain.py network=sub2_basic name=sub2_basic
+python train.py network=sub2_tuned name=sub2
